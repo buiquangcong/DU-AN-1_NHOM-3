76ac02ce02dabf9635304d97caf9699f89699073
buiquangcong <buicong908@gmail.com>
2025-11-27 00:22:18 +0700

    client

--- commons/env.php
+++ commons/env.php
@@ -2,6 +2,9 @@
 // ===== Biến môi trường hệ thống =====
 define('BASE_URL', 'http://localhost/DU-AN-1_NHOM-3/');
 define('BASE_URL_ADMIN', 'http://localhost/DU-AN-1_NHOM-3/admin/');
+define('BASE_URL_HDV', 'http://localhost/DU-AN-1_NHOM-3/hdv/');
+
+
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
